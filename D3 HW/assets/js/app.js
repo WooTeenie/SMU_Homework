0ca86bd5c640e480eef5f69068cae931c7cc9b9b
unknown <grant.daniel.wooldridge@gmail.com>
2021-02-12 14:05:29 -0600

    Initial Commit of Leaflet HW

--- D3 HW/assets/js/app.js
+++ D3 HW/assets/js/app.js
@@ -1,3 +1,7 @@
+// global
+var chosen_xaxis = "age";
+var chosen_yaxis = "smokes";
+
 $(document).ready(function() {
     makePlot();
 
@@ -14,15 +18,14 @@ function makePlot() {
         // STEP 1: SET UP THE CANVAS
         $("#scatter").empty();
 
-        // var svgWidth = 960;
         var svgWidth = window.innerWidth;
-        var svgHeight = 500;
+        var svgHeight = 600;
 
         var margin = {
             top: 20,
             right: 40,
-            bottom: 60,
-            left: 50
+            bottom: 100,
+            left: 80
         };
 
         var chart_width = svgWidth - margin.left - margin.right;
@@ -41,32 +44,31 @@ function makePlot() {
 
         // STEP 3: PREPARE THE DATA
         data.forEach(function(row) {
+            row.poverty = +row.poverty;
+            row.healthcare = +row.healthcare;
             row.age = +row.age;
+            row.income = +row.income;
             row.smokes = +row.smokes;
+            row.obesity = +row.obesity;
         });
 
         // STEP 4: Create the Scales
-        var xScale = d3.scaleLinear()
-            .domain(d3.extent(data, d => d.age))
-            .range([0, chart_width]);
-
-        var yScale = d3.scaleLinear()
-            .domain(d3.extent(data, d => d.smokes))
-            .range([chart_height, 0]);
+        var xScale = createXScale(data, chart_width);
+        var yScale = createYScale(data, chart_height);
 
         // STEP 5: CREATE THE AXES
         var leftAxis = d3.axisLeft(yScale);
         var bottomAxis = d3.axisBottom(xScale);
 
-        chart.append("g")
+        var xAxis = chart.append("g")
             .attr("transform", `translate(0, ${chart_height})`)
             .call(bottomAxis);
 
-        chart.append("g")
+        var yAxis = chart.append("g")
             .call(leftAxis);
 
         // STEP 5.5: Create the Text
-        var textVal = chart.append("g")
+        var textGroup = chart.append("g")
             .selectAll("text")
             .data(data)
             .enter()
@@ -78,82 +80,348 @@ function makePlot() {
 
         // STEP 6: CREATE THE GRAPH
         // append circles
-        var circles = chart.append("g")
+        var circlesGroup = chart.append("g")
             .selectAll("circle")
             .data(data)
             .enter()
             .append("circle")
             .style("opacity", 0.25)
             .attr("stroke-width", "1")
-            .classed("stateCircle", true)
-
+            .classed("stateCircle", true);
 
         // STEP 6.5: Be extra. Have the circles fly
         chart.selectAll("circle")
-            // .transition()
-            // .duration(5000)
-            .attr("cx", d => xScale(d.age))
-            .attr("cy", d => yScale(d.smokes))
+            .attr("cx", d => xScale(d[chosen_xaxis]))
+            .attr("cy", d => yScale(d[chosen_yaxis]))
             .attr("r", "15")
             .style("opacity", 0.25)
-            // .delay(function(d, i) { return i * 100 });
 
         chart.selectAll(".stateText")
-            // .transition()
-            // .duration(5000)
-            .attr("x", d => xScale(d.age))
-            .attr("y", d => yScale(d.smokes))
-            // .delay(function(d, i) { return i * 100 });
-
+            .attr("x", d => xScale(d[chosen_xaxis]))
+            .attr("y", d => yScale(d[chosen_yaxis]))
 
         // STEP 7: Add Axes Labels
         // Create axes labels
+
+        // CREATE FIRST XAXIS TEXT
+        chart.append("text")
+            .attr("transform", `translate(${chart_width / 2}, ${chart_height + margin.top + 30})`)
+            .attr("class", "axisText active")
+            .attr("id", "age") // new, create id to identify text
+            .text("Age (Median)")
+            .style("cursor", "pointer")
+            .on("click", function() {
+                chosen_xaxis = "age";
+
+                // update the x scale
+                xScale = createXScale(data, chart_width);
+
+                //update the x axis
+                bottomAxis = d3.axisBottom(xScale);
+                xAxis = createXAxis(xAxis, bottomAxis);
+
+                // update the GROUPS that we have on the graph
+                circlesGroup = updateCircles(circlesGroup, xScale, yScale);
+                textGroup = updateText(textGroup, xScale, yScale);
+
+                circlesGroup = createTooltip(circlesGroup);
+
+                d3.select(this).classed("inactive", false);
+                d3.select(this).classed("active", true);
+
+                d3.select("#poverty").classed("active", false);
+                d3.select("#poverty").classed("inactive", true);
+                d3.select("#income").classed("active", false);
+                d3.select("#income").classed("inactive", true);
+            });
+
+
+        // CREATE SECOND XAXIS TEXT
+        chart.append("text")
+            .attr("transform", `translate(${chart_width / 2}, ${chart_height + margin.top + 50})`)
+            .attr("class", "axisText inactive")
+            .attr("id", "poverty")
+            .text("Poverty (%)")
+            .style("cursor", "pointer")
+            .on("click", function() {
+                chosen_xaxis = "poverty";
+
+                // update the x scale
+                xScale = createXScale(data, chart_width);
+
+                //update the x axis
+                bottomAxis = d3.axisBottom(xScale);
+                xAxis = createXAxis(xAxis, bottomAxis);
+
+                // update the GROUPS that we have on the graph
+                circlesGroup = updateCircles(circlesGroup, xScale, yScale);
+                textGroup = updateText(textGroup, xScale, yScale);
+
+                circlesGroup = createTooltip(circlesGroup);
+
+                d3.select(this).classed("inactive", false);
+                d3.select(this).classed("active", true);
+
+                d3.select("#age").classed("active", false);
+                d3.select("#age").classed("inactive", true);
+                d3.select("#income").classed("active", false);
+                d3.select("#income").classed("inactive", true);
+            });
+
+        // CREATE THIRD XAXIS TEXT
+        chart.append("text")
+            .attr("transform", `translate(${chart_width / 2}, ${chart_height + margin.top + 70})`)
+            .attr("class", "axisText inactive")
+            .attr("id", "income")
+            .text("Household Income (Median)")
+            .style("cursor", "pointer")
+            .on("click", function() {
+                chosen_xaxis = "income";
+
+                // update the x scale
+                xScale = createXScale(data, chart_width);
+
+                //update the x axis
+                bottomAxis = d3.axisBottom(xScale);
+                xAxis = createXAxis(xAxis, bottomAxis);
+
+                // update the GROUPS that we have on the graph
+                circlesGroup = updateCircles(circlesGroup, xScale, yScale);
+                textGroup = updateText(textGroup, xScale, yScale);
+
+                circlesGroup = createTooltip(circlesGroup);
+
+                d3.select(this).classed("inactive", false);
+                d3.select(this).classed("active", true);
+
+                d3.select("#poverty").classed("active", false);
+                d3.select("#poverty").classed("inactive", true);
+                d3.select("#age").classed("active", false);
+                d3.select("#age").classed("inactive", true);
+            });
+
+
+        // CREATE FIRST YAXIS TEXT
+
         chart.append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 0 - margin.left + 0)
             .attr("x", 0 - (chart_height / 2))
             .attr("dy", "1em")
-            .attr("class", "axisText")
-            .text("Smokes (%)");
+            .attr("class", "axisText active")
+            .attr("id", "smokes") // new, create id to identify text
+            .text("Smokes (%)")
+            .style("cursor", "pointer")
+            .on("click", function() {
+                chosen_yaxis = "smokes";
 
+                // update the y scale
+                yScale = createYScale(data, chart_height);
+
+                //update the y axis
+                leftAxis = d3.axisLeft(yScale);
+                yAxis = createYAxis(yAxis, leftAxis);
+
+                // update the GROUPS that we have on the graph
+                circlesGroup = updateCircles(circlesGroup, xScale, yScale);
+                textGroup = updateText(textGroup, xScale, yScale);
+
+                circlesGroup = createTooltip(circlesGroup);
+
+                d3.select(this).classed("inactive", false);
+                d3.select(this).classed("active", true);
+
+                d3.select("#obesity").classed("active", false);
+                d3.select("#obesity").classed("inactive", true);
+                d3.select("#healthcare").classed("active", false);
+                d3.select("#healthcare").classed("inactive", true);
+            });
+
+
+        // CREATE SECOND YAXIS TEXT
         chart.append("text")
-            .attr("transform", `translate(${chart_width / 2}, ${chart_height + margin.top + 30})`)
-            .attr("class", "axisText")
-            .text("Age (Median)");
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left + 20)
+            .attr("x", 0 - (chart_height / 2))
+            .attr("dy", "1em")
+            .attr("class", "axisText inactive")
+            .attr("id", "obesity") // new, create id to identify text
+            .text("Obese (%)")
+            .style("cursor", "pointer")
+            .on("click", function() {
+                chosen_yaxis = "obesity";
+
+                // update the y scale
+                yScale = createYScale(data, chart_height);
+
+                //update the y axis
+                leftAxis = d3.axisLeft(yScale);
+                yAxis = createYAxis(yAxis, leftAxis);
+
+                // update the GROUPS that we have on the graph
+                circlesGroup = updateCircles(circlesGroup, xScale, yScale);
+                textGroup = updateText(textGroup, xScale, yScale);
+
+                circlesGroup = createTooltip(circlesGroup);
+
+                d3.select(this).classed("inactive", false);
+                d3.select(this).classed("active", true);
+
+                d3.select("#smokes").classed("active", false);
+                d3.select("#smokes").classed("inactive", true);
+                d3.select("#healthcare").classed("active", false);
+                d3.select("#healthcare").classed("inactive", true);
+            });
+
+
+        // CREATE THIRD YAXIS TEXT
+        chart.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left + 40)
+            .attr("x", 0 - (chart_height / 2))
+            .attr("dy", "1em")
+            .attr("class", "axisText inactive")
+            .attr("id", "healthcare") // new, create id to identify text
+            .text("Lacks Healthcare (%)")
+            .style("cursor", "pointer")
+            .on("click", function() {
+                chosen_yaxis = "healthcare";
+
+                // update the y scale
+                yScale = createYScale(data, chart_height);
+
+                //update the y axis
+                leftAxis = d3.axisLeft(yScale);
+                yAxis = createYAxis(yAxis, leftAxis);
+
+                // update the GROUPS that we have on the graph
+                circlesGroup = updateCircles(circlesGroup, xScale, yScale);
+                textGroup = updateText(textGroup, xScale, yScale);
+
+                circlesGroup = createTooltip(circlesGroup);
+
+                d3.select(this).classed("inactive", false);
+                d3.select(this).classed("active", true);
+
+                d3.select("#obesity").classed("active", false);
+                d3.select("#obesity").classed("inactive", true);
+                d3.select("#smokes").classed("active", false);
+                d3.select("#smokes").classed("inactive", true);
+            });
 
         // STEP 8: TOOLTIP
-        // Step 1: Initialize Tooltip
-        var toolTip = d3.tip()
-            .attr("class", "d3-tip")
-            .offset([180, -60])
-            .html(function(d) {
-                return (`<strong>${d.state}<strong><hr><strong>Median Age: ${d.age}</strong><hr><strong>Percentage of Smokers: ${d.smokes}%</strong>`);
-            });
+        circlesGroup = createTooltip(circlesGroup);
 
-        // Step 2: Create the tooltip in chartGroup.
-        circles.call(toolTip);
-
-        // Step 3: Create "mouseover" event listener to display tooltip
-        circles.on("mouseover", function(event, d) {
-                toolTip.show(d, this);
-
-                //make bubbles big
-                d3.select(this)
-                    .transition()
-                    .duration(1000)
-                    .attr("r", 50);
-            })
-            // Step 4: Create "mouseout" event listener to hide tooltip
-            .on("mouseout", function(event, d) {
-                toolTip.hide(d);
-
-                d3.select(this)
-                    .transition()
-                    .duration(1000)
-                    .attr("r", 15);
-            });
 
     }).catch(function(error) {
         console.log(error);
     });
+}
+
+/////////////////////////HELPER FUNCTIONS////////////////////////////
+
+// create x axis scale based on chosen column
+function createXScale(data, chart_width) {
+    var xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d[chosen_xaxis]))
+        .range([0, chart_width]);
+
+    return xScale;
+}
+
+// transition the xaxis to the NEW chosen one
+function createXAxis(xAxis, bottomAxis) {
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomAxis);
+
+    return xAxis;
+}
+
+// create y axis scale based on chosen column
+function createYScale(data, chart_height) {
+    var yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d[chosen_yaxis]))
+        .range([0, chart_height]);
+
+    return yScale;
+}
+
+// transition the yaxis to the NEW chosen one
+function createYAxis(yAxis, leftAxis) {
+    yAxis.transition()
+        .duration(1000)
+        .call(leftAxis);
+
+    return yAxis;
+}
+
+function updateCircles(circlesGroup, xScale, yScale) {
+    circlesGroup.transition()
+        .duration(1000)
+        .attr("cx", d => xScale(d[chosen_xaxis]))
+        .attr("cy", d => yScale(d[chosen_yaxis]));
+    return circlesGroup;
+}
+
+function updateText(textGroup, xScale, yScale) {
+    textGroup.transition()
+        .duration(1000)
+        .attr("x", d => xScale(d[chosen_xaxis]))
+        .attr("y", d => yScale(d[chosen_yaxis]));
+    return textGroup;
+}
+
+function createTooltip(circlesGroup) {
+    //step 0, get label
+    var xlabel = "";
+    if (chosen_xaxis == "age") {
+        xlabel = "Age (Median)";
+    } else if (chosen_xaxis == "income") {
+        xlabel = "Household Income";
+    } else {
+        xlabel = "Poverty";
+    }
+
+    var ylabel = "";
+    if (chosen_yaxis == "smokes") {
+        ylabel = "Smokes (%)";
+    } else if (chosen_yaxis == "obesity") {
+        ylabel = "Obese (%)";
+    } else {
+        ylabel = "Lacks Healthcare (%)";
+    }
+
+    // Step 1: Initialize Tooltip
+    var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([180, -60])
+        .html(function(d) {
+            return (`<strong>${d.state}</strong><hr><strong>${xlabel}: ${d[chosen_xaxis]}</strong><hr><strong>${ylabel}: ${d[chosen_yaxis]}%</strong>`);
+        });
+
+    // Step 2: Create the tooltip in chartGroup.
+    circlesGroup.call(toolTip);
+
+    // Step 3: Create "mouseover" event listener to display tooltip
+    circlesGroup.on("mouseover", function(event, d) {
+            toolTip.show(d, this);
+
+            //make bubbles big
+            d3.select(this)
+                .transition()
+                .duration(1000)
+                .attr("r", 50);
+        })
+        // Step 4: Create "mouseout" event listener to hide tooltip
+        .on("mouseout", function(event, d) {
+            toolTip.hide(d);
+
+            d3.select(this)
+                .transition()
+                .duration(1000)
+                .attr("r", 15);
+        });
+
+    return circlesGroup; // this is important
 }
---
+++ Leaflet Mapping HW/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoid29vdGVlbmllIiwiYSI6ImNra2c1cXEwdTA3dXkyb25zaDVmZTVicjMifQ.IE-ZQuq8kW302RPkI52ukQ";
